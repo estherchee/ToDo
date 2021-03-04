83b6c38ce99be32b3b9f8272927ee8ed00bf8e2b
Esther Chee <pnchee@gmail.com>
2021-03-04 21:08:17 +0100

    update display of tasks

--- src/com/estherchee/todo/view/ShowTasksMenu.java
+++ src/com/estherchee/todo/view/ShowTasksMenu.java
@@ -1,10 +1,13 @@
 package com.estherchee.todo.view;
 
+import com.estherchee.todo.model.Task;
 import com.estherchee.todo.model.TaskCollection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ShowTasksMenu extends Menu {
     public ShowTasksMenu() {
@@ -21,19 +24,35 @@ public class ShowTasksMenu extends Menu {
         getChoices();
     }
 
+    private void displayTasks(List<Task> tasks) {
+        int tasksSize = tasks.size();
+        if (tasksSize > 0) {
+            AtomicInteger taskIndex = new AtomicInteger(0);
+            for (Task task : tasks) {
+                System.out.println(task);
+                if (taskIndex.get() + 1 < tasksSize) {
+                    showSeparator();
+                }
+                taskIndex.getAndIncrement();
+            }
+        } else {
+            System.out.println("There is not tasks to display.");
+        }
+    }
+
     @Override
     public int executeMenuSwitcher(int userChoice, int commandToExit, Scanner commandReader, TaskCollection todos) {
         switch (userChoice) {
             case 1:
                 showSeparator();
-                System.out.println(todos.getTasksByDate());
+                displayTasks(todos.getTasksByDate());
                 showSeparator();
                 System.out.println("Press enter to continue.");
                 commandReader.nextLine();
                 break;
             case 2:
                 showSeparator();
-                System.out.println(todos.getTasksByProject());
+                displayTasks(todos.getTasksByProject());
                 showSeparator();
                 System.out.println("Press enter to continue.");
                 commandReader.nextLine();
--- src/com/estherchee/todo/view/TextView.java
+++ src/com/estherchee/todo/view/TextView.java
@@ -12,9 +12,6 @@ public class TextView {
     public TextView() {
         commandReader = new Scanner(System.in);
         todos = new TaskCollection();
-        todos.addTask("Hello2", "2012-12-05", "personal");
-        todos.addTask("Hello", "2012-12-02", "abc");
-        todos.addTask("Hello3", "2012-12-03", "zasd");
         mainMenu = new MainMenu();
     }
 
